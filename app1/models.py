4c1c82b5628c18067c244b9e87c98cdfef4eb424
yoshifumi watanabe <yoshifumi_watanabe_aa@mail.toyota.co.jp>
2021-08-26 17:34:30 +0900

    v5

--- app1/models.py
+++ app1/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Touroku(models.Model):
+    name = models.CharField(max_length=30)
+    gender = models.BooleanField()
+    age = models.IntegerField(default = 0)
+
+    def __str__(self):
+        return '<ID:'+str(self.id)+'>  名前:'+self.name+' ('+str(self.age)+'歳)'
